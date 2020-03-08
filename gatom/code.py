7aa0de7b318757829e8f07a3ad13c026ea7a6b27
Mangesh Chaudhari <59109845+mangesh187@users.noreply.github.com>
2020-03-08 11:15:06 +0530

    Update code.py

--- gatom/code.py
+++ gatom/code.py
@@ -10,32 +10,14 @@ categorical_var = df.select_dtypes(include = 'object')
 print(categorical_var)
 numerical_var = df.select_dtypes(include = 'number')
 print(numerical_var)
-
-# code ends here
-
-
-# --------------
-# code starts here
 banks = bank.drop(['Loan_ID'],axis = 1)
 print(banks.isnull().sum())
 bank_mode = banks.mode().iloc[0]
 banks.fillna(bank_mode, inplace=True)
 print(banks.isnull().sum())
-#code ends here
-
-
-# --------------
-# Code starts here
-
-
-
-
 avg_loan_amount = pd.pivot_table(banks,index=["Gender","Married","Self_Employed"],values=["LoanAmount"],aggfunc=(np.mean))
 print(avg_loan_amount)
-# code ends here
 
-# --------------
-# code starts here
 # code for loan aprroved for self employed
 loan_approved_se = banks.loc[(banks["Self_Employed"]=="Yes")  & (banks["Loan_Status"]=="Y"), ["Loan_Status"]].count()
 print(loan_approved_se)
@@ -56,18 +38,10 @@ percentage_nse=percentage_nse[0]
 #print percentage of loan for non self employed
 print (percentage_nse)
 
-# code ends here
-
-
-# --------------
-# code starts here
 loan_term = banks['Loan_Amount_Term'].apply(lambda x:int(x)/12)
 big_loan_term =len(loan_term[loan_term >=25])
 print(big_loan_term)
-# code ends here
 
-
-# --------------
 # code starts here
 loan_groupby = banks.groupby(['Loan_Status'])
 columns=['ApplicantIncome','Credit_History']
