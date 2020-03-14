dd89f28ec09993a2826c0bbd2321eaaeb67ef026
Mangesh Chaudhari <59109845+mangesh187@users.noreply.github.com>
2020-03-14 08:38:22 +0530

    Update code.py

--- Superhero-Statistics/code.py
+++ Superhero-Statistics/code.py
@@ -5,17 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 #path of the data file- path
 data = pd.read_csv(path)
+
 #Code starts here
+
 data['Gender'].replace('-','Agender',inplace = True) 
 gender_count = data['Gender'].value_counts()
 gender_count.plot(kind = 'bar')
 plt.title('Gender Counts')
 # --------------
 #Code starts here
+
 alignment = data['Alignment'].value_counts()
 alignment.plot.pie(label='Character Alignment')
 # --------------
 #Code starts here
+
 sc_df = data[['Strength','Combat']].copy()
 sc_covariance = sc_df.cov().iloc[0,1]
 sc_strength = data['Strength'].std()
@@ -33,6 +37,7 @@ ic_pearson = ic_pearson.round(2)
 print(ic_pearson)
 # --------------
 #Code starts here
+
 total_high = data['Total'].quantile(0.99)
 print(total_high)
 super_best = data[data['Total'] > total_high]
@@ -41,6 +46,7 @@ super_best_names = list(super_best_names)
 print(super_best_names)
 # --------------
 #Code starts here
+
 fig ,(ax_1,ax_2,ax_3) = plt.subplots(3)
 ax_1.plot(data['Intelligence'])
 plt.title('Intelligene')
@@ -49,5 +55,5 @@ plt.title('Speed')
 ax_3.plot(data['Power'])
 plt.title('Power')
 
-
+# --------------
 
