b02475a2917917e7338629bb5ad59fd27d252ccd
Mangesh Chaudhari <59109845+mangesh187@users.noreply.github.com>
2020-04-13 08:24:51 +0530

    Update code.py

--- Superhero-Statistics/code.py
+++ Superhero-Statistics/code.py
@@ -1,26 +1,19 @@
 # --------------
 #Header files
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 #path of the data file- path
 data = pd.read_csv(path)
 
-#Code starts here
-
 data['Gender'].replace('-','Agender',inplace = True) 
 gender_count = data['Gender'].value_counts()
 gender_count.plot(kind = 'bar')
 plt.title('Gender Counts')
 
-# --------------
-#Code starts here
-
 alignment = data['Alignment'].value_counts()
 alignment.plot.pie(label='Character Alignment')
-
-# --------------
-#Code starts here
 
 sc_df = data[['Strength','Combat']].copy()
 sc_covariance = sc_df.cov().iloc[0,1]
@@ -39,20 +32,12 @@ ic_pearson = ic_covariance/(ic_combat*ic_intelligence)
 ic_pearson = ic_pearson.round(2)
 print(ic_pearson)
 
-# --------------
-
-#Code starts here
-
 total_high = data['Total'].quantile(0.99)
 print(total_high)
 super_best = data[data['Total'] > total_high]
 super_best_names = super_best['Name']
 super_best_names = list(super_best_names)
 print(super_best_names)
-
-# --------------
-
-#Code starts here
 
 fig ,(ax_1,ax_2,ax_3) = plt.subplots(3)
 ax_1.plot(data['Intelligence'])
